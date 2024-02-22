9b609512a636b5fc1eb5c5a55bd813911029cb17
Tanner Lee <tannerlee@mymail.mines.edu>
2024-02-21 22:23:11 -0700

    Added initial db

---
+++ vov/database/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Users(
+  username TEXT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE Cards(
+    id TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    rank INT NULL,
+    type TEXT NOT NULL,
+    class TEXT NULL,
+    attribute TEXT NULL,
+    cost INT NULL,
+    power INT NULL,
+    life INT NULL,
+    hand INT NULL,
+    text TEXT NULL
+);
+
+CREATE TABLE Decks(
+    id INT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NULL,
+    user TEXT NOT NULL,
+    favorite TEXT NULL,
+    FOREIGN KEY (favorite) REFERENCES Cards(id),
+    FOREIGN KEY (user) REFERENCES Cards(username)
+);
+
+CREATE TABLE DeckCards(
+    id INT NOT NULL PRIMARY KEY,
+    card TEXT NOT NULL,
+    deck INT NOT NULL,
+    FOREIGN KEY (card) REFERENCES Cards(id),
+    FOREIGN KEY (deck) REFERENCES Decks(id)
+);
